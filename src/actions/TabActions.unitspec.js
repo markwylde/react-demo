39f01c3ce172191fca3e1a3ea990fa7008d47562
Mark Wylde <mark.wylde@verl.com>
2015-09-10 13:18:57 +0100

    Added unit and e2e tests

---
+++ src/actions/TabActions.unitspec.js
@@ -0,0 +1,11 @@
+import chai from 'chai';
+chai.should();
+
+describe('TabAction', function() {
+
+  it('should test the unitspec', function() {
+    var test = 'hello';
+    test.should.equal('hello');
+  });
+
+});
