217aa5a3a8ae624b384341502b658c5a032bc88d
Mark Wylde <mark.wylde@verl.com>
2016-01-24 23:44:55 +0000

    Fixed multiple await issue

--- features/step_definitions/observation.js
+++ features/step_definitions/observation.js
@@ -3,11 +3,11 @@ import steps from '../support/asygen';
 
 const { then } = module.exports = steps();
 
-then(/^I should see the total amount of my bill$/, function *() {
+then(/^I should see the total amount of my bill$/, async function() {
   const { By } = this.webdriver;
 
-  const element = yield this.driver.findElement(By.css('.bill-total'));
-  const value = yield element.getAttribute('textContent');
+  const element = this.driver.findElement(By.css('.bill-total'));
+  const value = await element.getAttribute('textContent');
   expect(value).to.equal('£130.10');
 });
 
